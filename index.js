a999923d18a17e50873be2cbd45a40529bc9d47b
Passive-Coder <passiveuser369@gmail.com>
2025-05-25 22:04:48 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function play(){
+    var player1 = (Math.floor(Math.random()*6) + 1);
+    var player2 = (Math.floor(Math.random()*6) + 1);
+
+    document.querySelector(".img1").setAttribute("src", ("./images/dice" + player1 + ".png"));
+    document.querySelector(".img2").setAttribute("src", ("./images/dice" + player2 + ".png"));
+
+    if(player1 > player2){
+        document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+    }
+    else if(player1 < player2){
+        document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+    }
+    else{
+        document.querySelector("h1").innerHTML = "Draw";
+    }
+}
